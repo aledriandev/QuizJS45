68fb0553bf6a0e3306ed2d615525a141439edddf
AQP20171679 <alejandraadriantejada@gmail.com>
2017-07-03 14:31:01 -0500

    El 4 ya funciona

--- problemas_referencial.js
+++ problemas_referencial.js
@@ -54,24 +54,23 @@ function alreves(array){
   return reves.join(" ");
 }
 
-//Ejercicio 4 *solo corre del 1 al 5 (:c) rayos
+//Ejercicio 4 *ya funciona
 //funcion que realiza el dibujo
 function dibuja(n){
   var array = [];
   var string = "";
-  //llenamos un array con los elementos de la base del tringulo y lo transformamos en string
+  //llenamos un array con los elementos de la base del tringulo
   for (var i=1 ; i < n*2 ; i++){
     array.push(i);
   }
-  array=array.join("");
-  // agregamos espacios segun el numero ingresado y quitamos los elementos del string array creado
+  // agregamos espacios segun el numero ingresado y quitamos los elementos del array creado y lo volvemos string
   for(var i=0 ; i < n ; i++){
-    string += espacios(n-i) + array.substr(0,array[i*2]) + "\n";
-  }
+   string += espacios(n-i) + array.slice(0,array[i*2]).join("") + "</br>";
+ }
   //para la parte de abajo
   for(var i=n-2 ; i>=0 ; i--){
-    string += espacios(n-i) + array.substr(0,array[i*2]) + "\n";
-  }
+      string += espacios(n-i) + array.slice(0,array[i*2]).join("") + "</br>";
+    }
   return string;
 }
 //funcion para determinar los espacios
